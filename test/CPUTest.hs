5e871d49aa216c3d06bc953d7f65f690ecc477fd
Tim McIver <tim@timmciver.com>
2019-07-25 15:00:50 -0400

    Create a `load` function to make test setup in `CPUTest.hs` more succinct.

--- test/CPUTest.hs
+++ test/CPUTest.hs
@@ -5,31 +5,36 @@ import CPU
 import Bus as Bus
 import Computer
 import Control.Monad.Trans.State.Lazy
+import Data.Word (Word8, Word16)
 
 test = scope "Computer state transitions" $
          scope "LDA tests" $ tests
          [
            let
-             cpu = initializedCPU
-             hw = HW (AddressRange 0 2) [0xa9, 0x35]
-             Right bus = Bus.add hw Bus.empty
-             comp = Computer cpu bus
+             dat = [0xa9, 0x35]
+             comp@(Computer cpu bus) = load dat 0
              pc = cpuProgramCounter cpu
              expectedCPU = cpu { cpuRegA = 0x35 , cpuProgramCounter = pc + 2, cpuTotalCycles = 2 }
              expectedComp = Computer expectedCPU bus
            in
             test6502Program "LDA Immediate test" comp expectedComp
          , let
-             cpu = initializedCPU { cpuProgramCounter = 0x04 }
-             hw = HW (AddressRange 0 6) [0, 0, 0xaa, 0, 0xa5, 0x02]
-             Right bus = Bus.add hw Bus.empty
-             comp = Computer cpu bus
+             dat = [0, 0, 0xaa, 0, 0xa5, 0x02]
+             comp@(Computer cpu bus) = load dat 4
              pc = cpuProgramCounter cpu
              expectedCPU = cpu { cpuRegA = 0xaa , cpuProgramCounter = pc + 2, cpuTotalCycles = 3 }
              expectedComp = Computer expectedCPU bus
            in
             test6502Program "LDA ZeroPage test" comp expectedComp
          ]
+
+load :: [Word8] -> Word16 -> Computer
+load dat pc = let
+  cpu = initializedCPU { cpuProgramCounter = pc }
+  hw = HW (AddressRange 0 (fromIntegral (length dat))) dat
+  Right bus = Bus.add hw Bus.empty
+  in
+    Computer cpu bus
 
 test6502Program :: String    -- test message
                 -> Computer  -- Computer initial state
